01fc72358c885d314fe73ee9117be752704ea238
Jimy dc <Jimyvdachi@gmail.com>
2025-01-08 22:02:33 +0700

    update

--- pages/ass/InternPage.js
+++ pages/ass/InternPage.js
@@ -1,4 +1,4 @@
-const BasePage = require('./BasePage');
+const BasePage = require('../BasePage');
 const {By} = require('selenium-webdriver'); 
 
 class InternPage extends BasePage {
--- pages/ass/UnvSupPage.js
+++ pages/ass/UnvSupPage.js
@@ -1,4 +1,4 @@
-const BasePage = require('./BasePage');
+const BasePage = require('../BasePage');
 const {By} = require('selenium-webdriver'); 
 
 class UnvSupPage extends BasePage {
